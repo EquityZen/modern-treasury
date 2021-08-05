3560c293ef9b1481f362ed5860cdb5b8ee97a46d
ez-issam <issam.zeibak@equityzen.com>
2021-08-05 13:11:56 -0400

    chnaged release version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'modern_treasury',
     packages = ['modern_treasury'],
-    version = '0.1',
+    version = '0.0.1',
     license='MIT',
     description = 'Modern Treasury Client Library',
     author = 'Issam Zeibak',
     author_email = 'issam.zeibak@equityzen.com',
     url = 'https://github.com/equityzen/modern-treasury',
-    download_url = 'https://github.com/EquityZen/modern-treasury/archive/refs/tags/v_01.tar.gz',
+    download_url = 'https://github.com/EquityZen/modern_treasury/archive/refs/tags/v_001.tar.gz',
     keywords = ['Modern Treasury', 'Finance'],
     install_requires=['pytest', 'requests'],
     classifiers=[
