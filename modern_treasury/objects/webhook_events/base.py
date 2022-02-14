a47d34c26f24d4571f461e650b153594ad64b532
Yazan Yousefalhaj <yazan.alhaj@equityzen.com>
2022-02-14 17:29:06 +0200

    remove metadata

--- modern_treasury/objects/webhook_events/base.py
+++ modern_treasury/objects/webhook_events/base.py
@@ -1,33 +1,6 @@
 from decimal import Decimal
 
 
-class MetaData:
-    def __init__(self, json_data: dict):
-        self.json_data = json_data
-
-    @property
-    def payment_type(self):
-        return self.json_data.get("payment_type")
-
-    @property
-    def buyer_id(self):
-        buyer_id = self.json_data.get("buyer_id")
-        return int(buyer_id)
-
-    @property
-    def deal_id(self):
-        deal_id = self.json_data.get("deal_id", {})
-        return int(deal_id)
-
-    @property
-    def transfer_type(self):
-        return self.json_data.get("transfer_type")
-
-    @property
-    def money_movement_id(self):
-        return self.json_data.get("money_movement_id")
-
-
 class Data:
     def __init__(self, json_data):
         self.json_data = json_data
@@ -39,11 +12,6 @@ class Data:
     @property
     def status(self):
         return self.json_data.get("status")
-
-    @property
-    def metadata(self):
-        metadata_json = MetaData(self.json_data.get("metadata", {}))
-        return metadata_json
 
     @property
     def error_message(self):
