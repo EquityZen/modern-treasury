d5ae241f2ff9564ab6acfd1ba5b1ee2eae7c6af3
Yazan Yousefalhaj <yazan.alhaj@equityzen.com>
2022-02-14 18:03:20 +0200

    remove import

--- modern_treasury/objects/webhook_events/__init__.py
+++ modern_treasury/objects/webhook_events/__init__.py
@@ -1,3 +1,2 @@
-import imp
 from .incoming_payment_detail_event import IncomingPaymentDetailEvent, IncomingPaymentDetailEventData, IncomingPaymentDetailsEventStates
 from .payment_order_event import PaymentOrderData, PaymentOrderEvent, PaymentOrderEventStates
