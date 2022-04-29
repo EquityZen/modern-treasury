f81f9e6610a0eb4b32b44da055516c574a9b3f07
Yazan Yousefalhaj <yazan.alhaj@equityzen.com>
2022-04-29 18:01:47 +0300

    make AddressRequest fields nullable

--- modern_treasury/objects/request/address.py
+++ modern_treasury/objects/request/address.py
@@ -1,14 +1,15 @@
 from dataclasses import asdict, dataclass
+from typing import Optional
 
 
 @dataclass
 class AddressRequest:
-    line1: str
-    line2: str
-    locality: str
-    region: str
-    postal_code: str
-    country: str
+    line1: Optional[str]
+    line2: Optional[str]
+    locality: Optional[str]
+    region: Optional[str]
+    postal_code: Optional[str]
+    country: Optional[str]
 
     def to_json(self) -> dict:
         return asdict(self)
